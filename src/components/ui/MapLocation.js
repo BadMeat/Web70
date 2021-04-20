2e7f736bac785b86fd0064680294d5d468a0dc99
arif <arif@gitlab.lab>
2021-04-20 15:18:59 +0700

    add map location

--- src/components/ui/MapLocation.js
+++ src/components/ui/MapLocation.js
@@ -52,3 +52,31 @@
 // }
 
 // export default MapLocation;
+
+import React, { Component } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+class MapLocation extends Component {
+  render() {
+    return (
+      <div id="mapid">
+        <MapContainer
+          center={[-6.2415983692887105, 106.79680146908244]}
+          zoom={17}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-6.2415983692887105, 106.79680146908244]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    );
+  }
+}
+export default MapLocation;
