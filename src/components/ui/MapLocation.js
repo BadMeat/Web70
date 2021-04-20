bee0ceba153fa9e2ed93fa92c8c264cb489220b8
arif <arif@gitlab.lab>
2021-04-20 12:27:33 +0700

    Remove dependency maps

--- src/components/ui/MapLocation.js
+++ src/components/ui/MapLocation.js
@@ -1,54 +1,54 @@
-import React from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+// import React from "react";
+// import ReactMapGL, { Marker } from "react-map-gl";
 
-const REACT_APP_MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1IjoiYXJpZmtlYnVtZW4iLCJhIjoiY2p3MnlhN29vMDdrMTN6cDlrbDZsbzY5aSJ9.YfLFXdWgLc4B6rCw5nJiCQ";
+// const REACT_APP_MAPBOX_ACCESS_TOKEN =
+//   "pk.eyJ1IjoiYXJpZmtlYnVtZW4iLCJhIjoiY2p3MnlhN29vMDdrMTN6cDlrbDZsbzY5aSJ9.YfLFXdWgLc4B6rCw5nJiCQ";
 
-function MapLocation(props) {
-  const [viewport, setViewport] = React.useState({
-    latitude: -6.2415983692887105,
-    longitude: 106.79680146908244,
-    zoom: 16,
-  });
+// function MapLocation(props) {
+//   const [viewport, setViewport] = React.useState({
+//     latitude: -6.2415983692887105,
+//     longitude: 106.79680146908244,
+//     zoom: 16,
+//   });
 
-  return (
-    <ReactMapGL
-      {...viewport}
-      width="100%"
-      height="100%"
-      onViewportChange={(viewport) => setViewport(viewport)}
-      mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-    >
-      <Marker
-        latitude={-6.241438390686289}
-        longitude={106.79680146908244}
-        offsetTop={-70}
-      >
-        <div>
-          <div
-            style={{
-              justifyContent: "center",
-              display: "flex",
-            }}
-          >
-            <p>SMAN 70</p>
-          </div>
-          <div
-            style={{
-              justifyContent: "center",
-              display: "flex",
-            }}
-          >
-            <img
-              src={require("../../img/pin.png")}
-              alt="My Pin"
-              style={{ width: "50px", height: "50px" }}
-            />
-          </div>
-        </div>
-      </Marker>
-    </ReactMapGL>
-  );
-}
+//   return (
+//     <ReactMapGL
+//       {...viewport}
+//       width="100%"
+//       height="100%"
+//       onViewportChange={(viewport) => setViewport(viewport)}
+//       mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
+//     >
+//       <Marker
+//         latitude={-6.241438390686289}
+//         longitude={106.79680146908244}
+//         offsetTop={-70}
+//       >
+//         <div>
+//           <div
+//             style={{
+//               justifyContent: "center",
+//               display: "flex",
+//             }}
+//           >
+//             <p>SMAN 70</p>
+//           </div>
+//           <div
+//             style={{
+//               justifyContent: "center",
+//               display: "flex",
+//             }}
+//           >
+//             <img
+//               src={require("../../img/pin.png")}
+//               alt="My Pin"
+//               style={{ width: "50px", height: "50px" }}
+//             />
+//           </div>
+//         </div>
+//       </Marker>
+//     </ReactMapGL>
+//   );
+// }
 
-export default MapLocation;
+// export default MapLocation;
